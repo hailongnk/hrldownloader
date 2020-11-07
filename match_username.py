5bae8c0beda1342dab47df9759f7b75ce3edf32a
LongNKH <hailongnk@gmail.com>
2020-11-07 18:13:12 +0700

    matching by combinations

--- match_username.py
+++ match_username.py
@@ -1,4 +1,5 @@
 import sys
+import itertools
 
 nargs = len(sys.argv)
 if (nargs < 3):
@@ -12,6 +13,7 @@ if (nargs < 3):
 # Get source files
 print("Matching usernames from source files:")
 # intermediate list for further purposes
+usersmap = {}
 sources = []
 for i in range(1, nargs):
   print(" %d. %s" % (i, sys.argv[i]))
@@ -25,11 +27,25 @@ for f in sources:
   with open(f, 'r') as reader:
     current_users = reader.readline().split()
   users.append(set(current_users)) # cast to set
+  usersmap[f] = set(current_users)
   print(" total usernames: ", len(current_users))
   print(" first user:", current_users[0])
   print(" last user:", current_users[-1])
 
-# matching usernames
-print("=== matched among %d set(s) ===" % (len(users)))
-matched = set.intersection(*users)
-print(matched)
+# matching all
+# print("=== matched among %d set(s) ===" % (len(users)))
+# matched = set.intersection(*users)
+# print(matched)
+
+# matching by combinations
+for numSets in range(2, len(sources)+1):
+  for groups in itertools.combinations(sources, numSets):
+    print("\n=== matching between", groups, "===")
+    m = set.intersection(*[usersmap[g] for g in groups])
+    print("total matches:", len(m))
+    if len(m) <= 100:
+      print(m)
+    else:
+      print("(do not list, too many matches)",)
+
+sys.exit()
