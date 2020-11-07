fdd583ff5159638a1c6b3b8a295c98f1a0368364
LongNKH <hailongnk@gmail.com>
2020-11-07 11:38:37 +0700

    match with multiple source files

--- match_username.py
+++ match_username.py
@@ -1,38 +1,35 @@
 import sys
 
 nargs = len(sys.argv)
-if (nargs != 3):
-  print("usage: match_username.py source target")
-  print("   source      path to usernames source file")
-  print("   target      path to usernames target file")
+if (nargs < 3):
+  print("error: missing arguments....\n")
+  print("usage: match_username.py source_file_list")
+  print("   source      path to usernames source file list")
   print(" e.g: python match_username.py week1/q1.log week1/q2.log")
   sys.exit()
 
-# Get source and target files
-q1 = sys.argv[1]
-q2 = sys.argv[2]
-print("Matching usernames between %s and %s" % (q1, q2))
 
-print("Reading %s" % (q1))
-q1_users = []
-with open(q1, 'r') as reader:
-  q1_users = reader.readline().split()
+# Get source files
+print("Matching usernames from source files:")
+# intermediate list for further purposes
+sources = []
+for i in range(1, nargs):
+  print(" %d. %s" % (i, sys.argv[i]))
+  sources.append(sys.argv[i])
 
-print("Reading %s" % (q2))
-q2_users = []
-with open(q2, 'r') as reader:
-  q2_users = reader.readline().split()
+# parsing usernames
+users = []
+for f in sources:
+  print("=== Reading %s ===" % (f))
+  current_users = []
+  with open(f, 'r') as reader:
+    current_users = reader.readline().split()
+  users.append(set(current_users)) # cast to set
+  print(" total usernames: ", len(current_users))
+  print(" first user:", current_users[0])
+  print(" last user:", current_users[-1])
 
-print("=== %s ===" % (q1))
-print("total usernames: ", len(q1_users))
-print("first user:", q1_users[0])
-print("last user:", q1_users[-1])
-
-print("=== %s ===" % (q2))
-print("total usernames: ", len(q2_users))
-print("first user:", q2_users[0])
-print("last user:", q2_users[-1])
-
-print("=== matched ===")
-matched = set.intersection(set(q1_users), set(q2_users))
+# matching usernames
+print("=== matched among %d set(s) ===" % (len(users)))
+matched = set.intersection(*users)
 print(matched)
